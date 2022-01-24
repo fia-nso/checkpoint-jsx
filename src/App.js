72c2ce2e710e823b6b454db509df172c24387b20
fia-nso <popekleib@gmail.com>
2022-01-24 15:14:40 +0100

    mon projet

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div>
+      <div className="App"  style={{border: '1px noir',maxWidth:'100vw'}}>
+        <h1 className='title'>Mohamed salem</h1>
+          <img src="./aff.png"  alt='pope' />
+          <img src="images/cap.png" alt=''width={400} height={400} />
+          
+      
+      </div>
+    {/* { <commandes vidéo width={320} height={240}> 
+          <source src="maVideo.mp4" type="video/mp4" />
+        </commandes> } */}
+    </div>
+  );
+}
+
+
+
+export default App;
+
